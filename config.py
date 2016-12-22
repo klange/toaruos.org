96efa09c882d6f8baacfcba6e0596092a5f65060
Kevin Lange <klange@dakko.us>
2016-12-22 22:52:03 +0900

    Big new blog post and add pygment CSS that works

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ IGNORE_FILES = ['.*.swp','*.py']
 THEME = 'themes/toaru'
 
 SITEURL = 'http://toaruos.org'
+#SITEURL = 'http://localhost:8080'
 OUTPUT_PATH = '/home/klange/Projects/toaruos.org.build'
 TWITTER_CARD_ACCOUNT = 'kevinlange'
 GITHUB_ACCOUNT = 'klange'
